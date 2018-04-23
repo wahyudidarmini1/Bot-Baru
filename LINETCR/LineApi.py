6ece23de7b0639215a3feb0789df869ae44fbf0e
wahyudidarmini1 <37975743+wahyudidarmini1@users.noreply.github.com>
2018-04-23 13:37:52 +0700

    Update LineApi.py

--- LINETCR/LineApi.py
+++ LINETCR/LineApi.py
@@ -23,7 +23,7 @@ class LINE:
   def __init__(self):
     self.Talk = Talk()
     self._session = requests.session() 
-    self._headers = {'X-Line-Application': 'CHROMEOS\t1.4.17\tChrome_OS\t1'}    
+    self._headers = {'X-Line-Application': 'IOSIPAD\x097.14.0\x09iPhone_OS\x0910.12.0'}    
 
   def login(self, mail=None, passwd=None, cert=None, token=None, qr=False, callback=None):
     if callback is None:
@@ -43,9 +43,9 @@ class LINE:
     self.authToken = self.Talk.authToken
     self.cert = self.Talk.cert
     self._headers = {
-              'X-Line-Application': 'CHROMEOS\t1.4.17\tChrome_OS\t1', 
+              'X-Line-Application': 'IOSIPAD\x097.14.0\x09iPhone_OS\x0910.12.0', 
               'X-Line-Access': self.authToken, 
-              'User-Agent': 'Line/1.4.17'
+              'User-Agent': 'Line/6.0.0 iPad4,1 9.0.2'
    }
    
     self.Poll = Poll(self.authToken)
